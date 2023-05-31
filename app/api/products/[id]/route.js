a1478c78e03df93ada19ce4fabbdcb1ee821a746
tandev94 <tandev94@tandev94s-MacBook-Pro.local>
2023-06-01 01:21:45 +0700

    ok

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -6,7 +6,7 @@ export const GET = async (request, { params }) => {
     await connectToDB();
 
     const prompts = await Order.find({ creator: params.id }).populate(
-      "creator"
+      "creatorz"
     );
 
     return new Response(JSON.stringify(prompts), { status: 200 });
