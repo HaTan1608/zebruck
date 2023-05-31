ddc8fecb7f6d8318e5f64a6296b26f40cb66c550
tandev94 <tandev94@tandev94s-MacBook-Pro.local>
2023-06-01 01:13:31 +0700

    ok

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -1,14 +1,18 @@
 import Order from "@/models/order";
-import { connectToDB } from "@utils/database";
+import { connectToDB } from "@/utils/database";
 
 export const GET = async (request, { params }) => {
-    try {
-        await connectToDB()
+  try {
+    await connectToDB();
 
-        const prompts = await Order.find({ creator: params.id }).populate("creator")
+    const prompts = await Order.find({ creator: params.id }).populate(
+      "creator"
+    );
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
-    } catch (error) {
-        return new Response("Failed to fetch prompts created by user", { status: 500 })
-    }
-} 
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch prompts created by user", {
+      status: 500,
+    });
+  }
+};
