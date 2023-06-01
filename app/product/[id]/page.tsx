b42ccf29207bc0f84f2fff1fb1c80c149882dc2b
tandev94 <tandev94@tandev94s-MacBook-Pro.local>
2023-06-02 02:13:39 +0700

    ok

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,42 @@
+import ProductImage from "@/components/ProductImage";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+async function ProductPage({ params: { id } }: Props) {
+  console.log("1321321", id);
+
+  try {
+    console.log("1321321", id);
+    const res = await fetch(`https://www.zebrucks.com/api/products/${id}`);
+    const product: any = await res.json();
+
+    return (
+      <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-48 pb-10">
+        <ProductImage product={product} />
+
+        <div className="divide-y">
+          <div className="space-y-2 pb-8">
+            <h1 className="text-2xl md:text-4xl font-bold">{product.name}</h1>
+            <h2 className="text-gray-500 font-bold text-xl md:text-3xl">
+              ${product.price}
+            </h2>
+          </div>
+
+          <div className="pt-8">
+            <p className="text-xs md:text-sm">{product.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  } catch (error) {
+    console.log("1321321", id);
+    notFound();
+  }
+}
+
+export default ProductPage;
